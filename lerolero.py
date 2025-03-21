9a838c33a4a81a2b57e556dfc80b1c3ff94118ee
Pedro Faria <pedro.f.faria@usp.br>
2025-03-21 10:29:55 -0300

    modifica frase blabla

--- lerolero.py
+++ lerolero.py
@@ -10,7 +10,7 @@ import random
 parte1 = [
 	"O sistema em desenvolvimento",
 	"O novo protocolo de comunicação",
-	"O algoritmo otimizado"
+	"O algoritmo foi otimizado e"
 	]
 parte2 = [
 	"possui excelente desempenho",
